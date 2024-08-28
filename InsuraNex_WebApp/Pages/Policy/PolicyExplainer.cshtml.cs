36f891fe76ad4a9f6f80c5e67db08231277d340c
Bhuvinesh Kumar S <kumarbhuvinesh@gmail.com>
2024-08-28 19:17:16 +0530

    Added Policy explainer page

---
+++ InsuraNex_WebApp/Pages/Policy/PolicyExplainer.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Insuranex.Pages.Policy
+{
+    public class PolicyExplainerModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
